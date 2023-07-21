ca25625ee9443d13f394adf0fd9f9b8ead3a22cc
ireneBern <99.bernardino.ir@gmail.com>
2023-07-21 09:52:47 +0100

    Fix: Remove "colcon build" inside package.

--- irb120pe_detection/build/irb120pe_detection/CMakeFiles/irb120pe_detection_uninstall.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/irb120pe_detection_uninstall"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/irb120pe_detection_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- irb120pe_detection/build/irb120pe_detection/CMakeFiles/irb120pe_detection_uninstall.dir/compiler_depend.ts
+++
@@ -1,2 +0,0 @@
-# CMAKE generated file: DO NOT EDIT!
-# Timestamp file for custom commands dependencies management for irb120pe_detection_uninstall.

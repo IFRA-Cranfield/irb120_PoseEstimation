c1cecc789bfc722aea29ed89059d15040b25194a
MikelBueno <Mikel.Bueno@cranfield.ac.uk>
2023-10-31 16:49:26 +0000

    Upload gripper_Gz.py (not finished).

---
+++ irb120pe_detection/python/gripper_Gz.py
@@ -0,0 +1,225 @@
+#!/usr/bin/python3
+
+# =============================================================================== #
+#                                COPYRIGHT HERE                                   #
+# =============================================================================== #
+
+# gripper_Gz.py
+# This function opens/closes the Schunk EGP-64 gripper in the Gazebo Simulation, and activates the IFRA_LinkAttacher plugin if the gripper is about to grasp any object in the workspace.
+
+# ===== IMPORT REQUIRED COMPONENTS ===== #
+# Required to include ROS2 and its components:
+import rclpy
+from rclpy.node import Node
+from rclpy.action import ActionClient
+# Import /Move and /RobMove ROS2 Actions:
+from ros2srrc_data.action import Move
+# Import ROS2 messages:
+from std_msgs.msg import String
+from ros2srrc_data.msg import Action
+from objectpose_msgs.msg import ObjectPose
+from linkpose_msgs.msg import LinkPose
+
+# GLOBAL VARIABLE -> CUBES:
+CUBES = {}
+CUBES["WhiteCube"] = {"x": 0.0, "y": 0.0, "z": 0.0, "qx": 0.0, "qy": 0.0, "qz": 0.0, "qw": 0.0}
+CUBES["BlackCube"] = {"x": 0.0, "y": 0.0, "z": 0.0, "qx": 0.0, "qy": 0.0, "qz": 0.0, "qw": 0.0}
+CUBES["WhiteCube"] = {"x": 0.0, "y": 0.0, "z": 0.0, "qx": 0.0, "qy": 0.0, "qz": 0.0, "qw": 0.0}
+CUBES["White"] = {"x": 0.0, "y": 0.0, "z": 0.0, "qx": 0.0, "qy": 0.0, "qz": 0.0, "qw": 0.0}
+
+# GLOBAL VARIABLE -> EE_POSE:
+EE_POSE = LinkPose()
+
+# =============================================================================== #
+# /Move ACTION CLIENT:
+
+class MoveCLIENT(Node):
+
+    def __init__(self):
+
+        super().__init__('GzGripper_Move_Client')
+        self._action_client = ActionClient(self, Move, 'Move')
+
+        self.get_logger().info("(/Move)-Gripper: Initialising ROS2 Action Client!")
+        self.get_logger().info("(/Move)-Gripper: Waiting for /Move ROS2 ActionServer to be available...")
+        self._action_client.wait_for_server()
+        self.get_logger().info("(/Move)-Gripper: /Move ACTION SERVER detected.")
+
+    def send_goal(self, ACTION):
+
+        goal_msg = Move.Goal()
+        goal_msg.action = ACTION.action
+        goal_msg.speed = ACTION.speed
+        goal_msg.moveg = ACTION.moveg
+        
+        self._send_goal_future = self._action_client.send_goal_async(goal_msg)
+        self._send_goal_future.add_done_callback(self.goal_response_callback)
+
+    def goal_response_callback(self, future):
+        
+        goal_handle = future.result()
+
+        if not goal_handle.accepted:
+            self.get_logger().info('(/Move)-Gripper: Move ACTION CALL -> GOAL has been REJECTED.')
+            return
+        
+        # self.get_logger().info('(/Move): Move ACTION CALL -> GOAL has been ACCEPTED.')
+
+        self._get_result_future = goal_handle.get_result_async()
+        self._get_result_future.add_done_callback(self.get_result_callback)
+
+    def get_result_callback(self, future):
+        
+        global RES
+        
+        RESULT = future.result().result
+        RES.MESSAGE = RESULT.result
+
+        if "FAILED" in RES.MESSAGE:
+            RES.SUCCESS = False
+        else:
+            RES.SUCCESS = True
+
+# =============================================================================== #
+# CubePose SUBSCRIBER:
+
+class CubePose(Node):
+
+    def __init__(self):
+
+        super().__init__("irb120pe_CubePose_Subscriber")
+        self.SUBWhite = self.create_subscription(ObjectPose, "/WhiteCube/ObjectPose", self.CALLBACK_FN, 10)
+        self.SUBBlack = self.create_subscription(ObjectPose, "/BlackCube/ObjectPose", self.CALLBACK_FN, 10)
+        self.SUBBlue = self.create_subscription(ObjectPose, "/BlueCube/ObjectPose", self.CALLBACK_FN, 10)
+        self.SUBCube = self.create_subscription(ObjectPose, "/Cube/ObjectPose", self.CALLBACK_FN, 10)
+
+    def CALLBACK_FN(self, OBJ):
+
+        global CUBES
+
+        for Cube, Pose in CUBES.items():
+
+            if (OBJ.objectname == Cube):
+
+                Pose["x"] = OBJ.x
+                Pose["y"] = OBJ.x
+                Pose["z"] = OBJ.x
+                Pose["qx"] = OBJ.x
+                Pose["qy"] = OBJ.x
+                Pose["qz"] = OBJ.x
+                Pose["qw"] = OBJ.x
+
+# =============================================================================== #
+# EEPose SUBSCRIBER:
+
+class EEPose(Node):
+
+    def __init__(self):
+
+        super().__init__("irb120pe_CubePose_Subscriber")
+        self.SUB_EE = self.create_subscription(LinkPose, "/LinkPose_irb120_EE_egp64", self.CALLBACK_FN, 10)
+
+    def CALLBACK_FN(self, EE):
+
+        global EE_POSE
+        EE_POSE = EE
+
+# =============================================================================== #
+# GzGripper class, to OPEN/CLOSE the Gripper in Gazebo:
+
+class GzGripper():
+
+    def __init__(self):
+
+        # Initialise /Move Action Client:
+        self.MoveClient = MoveCLIENT()
+
+        # Initialise EEPose and CubePose classes:
+        self.CubeLocation = CubePose()
+        self.EEPose = EEPose()
+
+    def CLOSE(self):
+
+        WP = Action()
+        WP.action = "MoveG"
+        WP.speed = 1.0
+        WP.moveg = 0.04
+
+        self.MoveClient.send_goal(WP)
+
+        while rclpy.ok():
+
+            rclpy.spin_once(self.MoveClient)
+
+            if (RES.MESSAGE != "null"):
+                break
+
+        if (RES.SUCCESS == True):
+            
+            print("Result -> " + RES.MESSAGE)
+            print("")
+            
+            RES.MESSAGE = "null"
+            RES.SUCCESS = False
+
+        elif (RES.SUCCESS == False):
+    
+            print("Result -> " + RES.MESSAGE)
+
+            rclpy.shutdown()
+            print("CLOSING PROGRAM...")
+            exit()
+
+        # ===== CHECK GRASPING ===== #
+
+        global CUBES
+        global EE_POSE
+
+        rclpy.spin_once(self.CubeLocation)
+        rclpy.spin_once(self.EEPose)
+
+        # 1. Check which CUBE is in workspace, and get its POSE:
+        OBJ = ObjectPose()
+        Found = False
+        for Cube, Pose in CUBES.items():
+
+            for VALUE in Pose.values():
+
+                if VALUE != 0.0:
+                    Found = True
+                    break
+
+            if Found:
+                OBJ.objectname = Cube
+                OBJ.x = Pose["x"]
+                OBJ.y = Pose["y"]
+                OBJ.z = Pose["z"]
+                OBJ.qx = Pose["qx"]
+                OBJ.qy = Pose["qy"]
+                OBJ.qz = Pose["qz"]
+                OBJ.qw = Pose["qw"]
+                break
+
+        if not Found:
+            print("(GzGripper): Gripper closed without grasping any object.")
+            return()
+        
+        # 2. Check if CubePose and EEPose match:
+        AttachCheck = True
+        if (EE_POSE.x - 0.01 > OBJ.x) or (EE_POSE.x + 0.01 < OBJ.x):
+            AttachCheck = False
+        if (EE_POSE.y - 0.01 > OBJ.y) or (EE_POSE.y + 0.01 < OBJ.y):
+            AttachCheck = False
+        if (EE_POSE.z - 0.01 > OBJ.z) or (EE_POSE.z + 0.01 < OBJ.z):
+            AttachCheck = False
+
+        # 3. ATTACH if Check=True:
+        if AttachCheck:
+            
+            # LinkAttacher:
+            None
+
+            print("(GzGripper): Gripper closed, object->" + str(OBJ.objectname) + " attached.")
+
+        else:
+            print("(GzGripper): Gripper closed, object->" + str(OBJ.objectname) + " not attached.")
